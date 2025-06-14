14eccc2c41bd47f835bb8c62a94bc9194569c776
sasakimirei <hinatazaka46n@gmail.com>
2025-06-14 22:21:53 +0700

    vuex hoc het video 11

--- 15 - Vuex/src/main.js
+++ 15 - Vuex/src/main.js
@@ -1,7 +1,66 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
 
-import App from './App.vue';
+import App from './App.vue'
 
-const app = createApp(App);
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+      isLoggedIn: false,
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.counter += 2
+    },
+    increase(state, payload) {
+      state.counter += payload.value
+    },
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth
+    },
+  },
+  actions: {
+    increment(context) {
+      setTimeout(() => {
+        context.commit('increment')
+      }, 2000)
+    },
+    increase(context, payload) {
+      console.log(context)
 
-app.mount('#app');
+      context.commit('increase', payload)
+    },
+    login(context) {
+      context.commit('setAuth', { isAuth: true })
+    },
+    logout(context) {
+      context.commit('setAuth', { isAuth: false })
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter
+      if (finalCounter < 0) {
+        return 0
+      }
+      if (finalCounter > 100) {
+        return 100
+      }
+      return finalCounter
+    },
+    userIsAuthenticated(state) {
+      return state.isLoggedIn
+    },
+  },
+})
+
+const app = createApp(App)
+
+app.use(store)
+
+app.mount('#app')
